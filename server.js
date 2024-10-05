a313aa1534c74cd824b96430472ee433acd5b788
NathanEstudante <nptestudante@gmail.com>
2024-10-05 15:22:25 -0300

    Delete EJS module from project Use HTML as view engine instead EJS

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
