b125e4034f677e188e4e6caed0168f7b789c1219
NathanEstudante <nptestudante@gmail.com>
2024-10-05 15:05:26 -0300

    Create game back-end file and config Use express to create game back-end on localhost:3000, set view engine and initial page

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
