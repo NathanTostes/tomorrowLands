8473c31f2872dc1c5d02ba8ee8f9e277bf306daa
NathanEstudante <nptestudante@gmail.com>
2024-10-06 10:08:35 -0300

    Create enemy class and movement Enemy class: - static array for alive enemies - element atribute as html div reference - life and velocity attributes - moveToPlayer() function for create enemy logic Then solving issue #1, with the player being chased by enemies, and solving issue #2, with the enemy chasing system

---
+++ public/classes/Enemy.js
@@ -0,0 +1,60 @@
+class Enemy {
+    static aliveEnemies = [];
+
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.classList.add('enemy');
+        
+        const gameContainer = document.getElementById('gameContainer');
+        const enemySize = parseInt(getComputedStyle(this.element).getPropertyValue('--enemy-size')) || 30;
+        this.velocity = 2;
+        this.life = 1;
+        this.enemySize = enemySize;
+        
+        let x, y;
+        const spawnDirection = Math.floor(Math.random() * 4);
+        switch (spawnDirection) {
+            case 0: // Topo
+                x = Math.random() * (gameContainer.clientWidth - enemySize);
+                y = -enemySize;
+                break;
+            case 1: // Direita
+                x = gameContainer.clientWidth;
+                y = Math.random() * (gameContainer.clientHeight - enemySize);
+                break;
+            case 2: // Fundo
+                x = Math.random() * (gameContainer.clientWidth - enemySize);
+                y = gameContainer.clientHeight;
+                break;
+            case 3: // Esquerda
+                x = -enemySize;
+                y = Math.random() * (gameContainer.clientHeight - enemySize);
+                break;
+        }
+
+        this.element.style.left = `${x}px`;
+        this.element.style.top = `${y}px`;
+        gameContainer.appendChild(this.element);
+    }
+
+    moveToPlayer(playerDirection) {
+        const rectEnemy = this.element.getBoundingClientRect();
+        const enemyCenterX = rectEnemy.left + rectEnemy.width / 2;
+        const enemyCenterY = rectEnemy.top + rectEnemy.height / 2;
+        const playerCenterX = playerDirection.left + playerDirection.width / 2;
+        const playerCenterY = playerDirection.top + playerDirection.height / 2;
+
+        const deltaX = playerCenterX - enemyCenterX;
+        const deltaY = playerCenterY - enemyCenterY;
+        const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        if (distance > 0) {
+            const moveX = (deltaX / distance) * this.velocity;
+            const moveY = (deltaY / distance) * this.velocity;
+            this.element.style.left = (rectEnemy.left + moveX) + 'px';
+            this.element.style.top = (rectEnemy.top + moveY) + 'px';
+        }
+    }
+}
+
+export default Enemy;
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,3 +1,20 @@
 import Player from '/classes/Player.js';
+import Enemy from '/classes/Enemy.js';
 
 Player.create();
+
+function spawnEnemy() {
+    const enemy = new Enemy();
+    Enemy.aliveEnemies.push(enemy);
+}
+
+function gameLoop() {
+    const playerRect = Player.element.getBoundingClientRect();
+    Enemy.aliveEnemies.forEach((enemy) => {
+        enemy.moveToPlayer(playerRect);
+    });
+    requestAnimationFrame(gameLoop);
+}
+
+setInterval(spawnEnemy, 1000);
+requestAnimationFrame(gameLoop);
