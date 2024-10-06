a534528547cacd8489f59c3b4129b2e325b6b2d6
NathanEstudante <nptestudante@gmail.com>
2024-10-06 08:33:28 -0300

    Use EJS view engine and import Express Define EJS view engine in server.js file and import Express again

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'temp')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Tomorrow Lands' });
 });
 
 const PORT = 3000;
