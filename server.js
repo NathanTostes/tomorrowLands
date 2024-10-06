130fe72a8776d953af9d25da786109ae589f544a
NathanEstudante <nptestudante@gmail.com>
2024-10-06 08:58:55 -0300

    Set initial game front-end Create a EJS game view, with player and set a CSS style

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'temp')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Tomorrow Lands' });
