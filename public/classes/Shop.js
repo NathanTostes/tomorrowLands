ef59e020499b0c84c2c109c56a80a2ded1f0cff6
LeoHAHA <cardosoleo960@gmail.com>
2024-10-11 10:13:37 -0300

    Created a system that adds the elements of the shopOptions constant
    Adds an itemDiv for each item in the shopOptions constant - Issue-#8

--- public/classes/Shop.js
+++ public/classes/Shop.js
@@ -1,11 +1,22 @@
 import Round from "./Round.js";
 const gameContainer = document.getElementById('gameContainer')
 
+const shopOptions = {
+    item1: {value: 3, description: 'item1'},
+    item2: {value: 5, description: 'item2'},
+}
+
+
 class Shop {
     constructor() {
+        this.createElements()
+    }
+
+    createElements(){
         this.createElementShop()
         this.createTitle()
         this.createButtonExit()
+        this.createShopItems()
     }
 
     createElementShop(){
@@ -14,6 +25,18 @@ class Shop {
         gameContainer.appendChild(this.elementShop);
 
     }
+
+    createShopItems() {
+ 
+        Object.keys(shopOptions).forEach((itemKey) => {
+            const item = shopOptions[itemKey];            
+            const itemDiv = document.createElement('div');
+            itemDiv.classList.add('shop-item');        
+            itemDiv.innerHTML = `<strong>${item.description}</strong><br>Valor: ${item.value}`;
+            this.elementShop.appendChild(itemDiv);
+        });
+    }
+
 
     createTitle() {
         const title = document.createElement('p')
